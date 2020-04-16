2745cd8c03e6c9525f19f3b338c0d331682eac45
tazlambert <54355516+tazlambert@users.noreply.github.com>
2020-04-16 23:33:44 +0700

    Update model.py

--- scripts/model.py
+++ scripts/model.py
@@ -103,11 +103,11 @@ dep.setSourcePath("wlsdeploy/applications/opdemo.war")
 
 # Deploy WLS Exporter tools
 # =========================
-#cd("/")
-#dep=create("wls-exporter", "AppDeployment")
-#dep.setTargets(jarray.array([cl],TargetMBean))
-#dep.setModuleType("war")
-#dep.setSourcePath("wlsdeploy/applications/wls-exporter.war")
+cd("/")
+dep=create("wls-exporter", "AppDeployment")
+dep.setTargets(jarray.array([cl],TargetMBean))
+dep.setModuleType("war")
+dep.setSourcePath("wlsdeploy/applications/wls-exporter.war")
 
 writeDomain(domain_path)
 closeTemplate()
