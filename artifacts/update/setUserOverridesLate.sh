b464de9a1dd8a6f24f030098d10c86a7b3d00a09
tazlambert <54355516+tazlambert@users.noreply.github.com>
2020-05-09 13:25:07 +0700

    Add files via upload

---
+++ artifacts/update/setUserOverridesLate.sh
@@ -0,0 +1,1 @@
+export CLASSPATH="/u01/oracle/user_projects/domains/wls-k8s-domain/lib/weblogic-logging-exporter-1.0.0.jar:/u01/oracle/user_projects/domains/wls-k8s-domain/lib/snakeyaml-1.23.jar:$CLASSPATH"
