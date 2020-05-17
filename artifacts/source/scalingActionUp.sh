16e52b37a96ff12870a44bc40613af910c2ee7a7
tazlambert <54355516+tazlambert@users.noreply.github.com>
2020-05-17 15:13:49 +0700

    Update scalingActionUp.sh

--- artifacts/source/scalingActionUp.sh
+++ artifacts/source/scalingActionUp.sh
@@ -4,12 +4,6 @@
 
 echo "called scalingActionUp.sh" >> scalingActionUp.log
 
-# environment variable from admin-server pod ENV
-
-KUBERNETES_SERVICE_HOST=10.96.0.1
-KUBERNETES_SERVICE_PORT=443
-INTERNAL_OPERATOR_CERT=LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUR5ekNDQXJPZ0F3SUJBZ0lFYkdpTVREQU5CZ2txaGtpRzl3MEJBUXNGQURBY01Sb3dHQVlEVlFRREV4RjMNClpXSnNiMmRwWXkxdmNHVnlZWFJ2Y2pBZUZ3MHlNREExTVRBd05qRTFOVE5hRncwek1EQTFNRGd3TmpFMU5UTmENCk1Cd3hHakFZQmdOVkJBTVRFWGRsWW14dloybGpMVzl3WlhKaGRHOXlNSUlCSWpBTkJna3Foa2lHOXcwQkFRRUYNCkFBT0NBUThBTUlJQkNnS0NBUUVBcjhkei9ybFMrb2xyTEdacWdSWW1OL3crY0JaRXg4WU0wY2Z2RTY2dlcwa00NCjVja1c3LzFaS0g1SmlzTmlXWHQ4dkpWVTZhbnlBSE51WnpzMnBGT2NydVJMcld0djljK0oyTFg5WkR4N2s0WFcNCjZyR09ObkRoem40c1BCZ0crRCs3M0VMUW4zVkhoR3NwbWZua3JqdEFOSzRGczJqT0Jib0hNa0hLYWIrUTZSb0ENCmhVL2VFT21NczJEcDg1TmlTdXFlTDNVeFFURFZ2ZTJRRlhlWWdyQkl6ODJ4OWZkcXJOem5pN0toU09BbXpxOCsNCmtlL3hlYlU3OFVDMzR3MjZYWGNnVHI0RmdPTkpCaGdpZDZqUVl5V2RoTDRoZWRiUUM4dWtkaUJvak1PdFl1bE8NCmoxdUs3S2c1Z3lEcjF4cktBMlpUY01OM2NkTzFZUHVqL3BRcnNjQU45UUlEQVFBQm80SUJFekNDQVE4d0hRWUQNClZSME9CQllFRlBoaUY2ejYxaHIrYUtpN3NlSUdobGhDR0hLaU1Bc0dBMVVkRHdRRUF3SUQrRENCNEFZRFZSMFINCkJJSFlNSUhWZ2g1cGJuUmxjbTVoYkMxM1pXSnNiMmRwWXkxdmNHVnlZWFJ2Y2kxemRtT0NNMmx1ZEdWeWJtRnMNCkxYZGxZbXh2WjJsakxXOXdaWEpoZEc5eUxYTjJZeTUzWldKc2IyZHBZeTF2Y0dWeVlYUnZjaTF1YzRJM2FXNTANClpYSnVZV3d0ZDJWaWJHOW5hV010YjNCbGNtRjBiM0l0YzNaakxuZGxZbXh2WjJsakxXOXdaWEpoZEc5eUxXNXoNCkxuTjJZNEpGYVc1MFpYSnVZV3d0ZDJWaWJHOW5hV010YjNCbGNtRjBiM0l0YzNaakxuZGxZbXh2WjJsakxXOXcNClpYSmhkRzl5TFc1ekxuTjJZeTVqYkhWemRHVnlMbXh2WTJGc01BMEdDU3FHU0liM0RRRUJDd1VBQTRJQkFRQUENCjdzMnB5dk1FSG9lTGR6ZGN1NHk1VmtBZWdiVHlTR2MwTWUwUkR0elNBUVB6N0lCQllNLzg0R2ZkZGlmVWxwSkUNCnM1RGdmOWcxb2hzN0JBWDNZNlFOTVI3c2tDQ2o2OEUzK0RRSkpBUUUvbXhEak9mTnBvUGl3Z1QzMUcxSzdmelANCk40OUQ4Y09VZjNyTmVDaWhvREV6dDRROFlNb0RybHd6WTNzVnRDZXRLdzV3d1h1Ry9FakI2dEhwL2FXeGFSTE8NCndmMVN6RmVBTllmMDdrVkl4ZE5wbFV5RFdLYVN4VUhEYnhvM3d0YzBaQm1sU1VYNEFCZlRtOU1HZzZOcGp1ZlkNClpEOUVDNE40T2xGU0REYmpLSXBHL0Z0RTJXdHR4cHBrbGZLSDdzS3I0V1JlY2hCclBsNk9MNDVJUlFCdkVSZDkNCnhSZlNVWEJ2bFQ5Q3BHcG1ZZmN4Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K
-
 # script parameters
 scaling_action="scaleUp"
 wls_domain_uid="wls-k8s-domain"
@@ -96,21 +90,21 @@ then
   access_token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
 fi
 
-echo "scaling_action: $scaling_action" >> scalingAction.log
-echo "wls_domain_uid: $wls_domain_uid" >> scalingAction.log
-echo "wls_cluster_name: $wls_cluster_name" >> scalingAction.log
-echo "wls_domain_namespace: $wls_domain_namespace" >> scalingAction.log
-echo "operator_service_name: $operator_service_name" >> scalingAction.log
-echo "operator_service_account: $operator_service_account" >> scalingAction.log
-echo "operator_namespace: $operator_namespace" >> scalingAction.log
-echo "scaling_size: $scaling_size" >> scalingAction.log
+echo "scaling_action: $scaling_action" >> scalingActionUp.log
+echo "wls_domain_uid: $wls_domain_uid" >> scalingActionUp.log
+echo "wls_cluster_name: $wls_cluster_name" >> scalingActionUp.log
+echo "wls_domain_namespace: $wls_domain_namespace" >> scalingActionUp.log
+echo "operator_service_name: $operator_service_name" >> scalingActionUp.log
+echo "operator_service_account: $operator_service_account" >> scalingActionUp.log
+echo "operator_namespace: $operator_namespace" >> scalingActionUp.log
+echo "scaling_size: $scaling_size" >> scalingActionUp.log
 
 # Query WebLogic Operator Service Port
 STATUS=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -X GET $kubernetes_master/api/v1/namespaces/$operator_namespace/services/$operator_service_name/status` 
 if [ $? -ne 0 ]
   then
-    echo "Failed to retrieve status of $operator_service_name in name space: $operator_namespace" >> scalingAction.log
-    echo "STATUS: $STATUS" >> scalingAction.log
+    echo "Failed to retrieve status of $operator_service_name in name space: $operator_namespace" >> scalingActionUp.log
+    echo "STATUS: $STATUS" >> scalingActionUp.log
     exit 1
 fi
 
@@ -121,14 +115,14 @@ for i in json.load(sys.stdin)["spec"]["ports"]:
     print(i["port"])
 INPUT
 port=`echo ${STATUS} | python cmds.py`
-echo "port: $port" >> scalingAction.log
+echo "port: $port" >> scalingActionUp.log
 
 # Retrieve Custom Resource Definition for WebLogic domain
 CRD=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -X GET $kubernetes_master/apis/apiextensions.k8s.io/v1beta1/customresourcedefinitions/domains.weblogic.oracle`
 if [ $? -ne 0 ]
   then
-    echo "Failed to retrieve Custom Resource Definition for WebLogic domain" >> scalingAction.log
-    echo "CRD: $CRD" >> scalingAction.log
+    echo "Failed to retrieve Custom Resource Definition for WebLogic domain" >> scalingActionUp.log
+    echo "CRD: $CRD" >> scalingActionUp.log
     exit 1
 fi
 
@@ -138,17 +132,17 @@ import sys, json
 print(json.load(sys.stdin)["spec"]["version"])
 INPUT
 domain_api_version=`echo ${CRD} | python cmds.py`
-echo "domain_api_version: $domain_api_version" >> scalingAction.log
+echo "domain_api_version: $domain_api_version" >> scalingActionUp.log
 
 # Reteive Custom Resource Domain 
 DOMAIN=`curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" $kubernetes_master/apis/weblogic.oracle/$domain_api_version/namespaces/$wls_domain_namespace/domains/$domain_uid`
 if [ $? -ne 0 ]
   then
-    echo "Failed to retrieve WebLogic Domain Custom Resource Definition" >> scalingAction.log
-    echo "DOMAIN: $DOMAIN" >> scalingAction.log
+    echo "Failed to retrieve WebLogic Domain Custom Resource Definition" >> scalingActionUp.log
+    echo "DOMAIN: $DOMAIN" >> scalingActionUp.log
     exit 1
 fi
-echo "DOMAIN: $DOMAIN" >> scalingAction.log
+echo "DOMAIN: $DOMAIN" >> scalingActionUp.log
 
 # Verify if cluster is defined in clusters
 cat > cmds.py << INPUT
@@ -171,7 +165,7 @@ in_cluster_startup=`echo ${DOMAIN} | python cmds.py`
 # or not.
 if [ $in_cluster_startup == "True" ]
 then
-  echo "$wls_cluster_name defined in clusters" >> scalingAction.log
+  echo "$wls_cluster_name defined in clusters" >> scalingActionUp.log
 
 cat > cmds.py << INPUT
 import sys, json
@@ -183,7 +177,7 @@ for i in json.load(sys.stdin)["items"]:
 INPUT
   num_ms=`echo ${DOMAIN} | python cmds.py`
 else
-  echo "$wls_cluster_name NOT defined in clusters" >> scalingAction.log
+  echo "$wls_cluster_name NOT defined in clusters" >> scalingActionUp.log
 cat > cmds.py << INPUT
 import sys, json
 for i in json.load(sys.stdin)["items"]:
@@ -191,7 +185,7 @@ for i in json.load(sys.stdin)["items"]:
 INPUT
   num_ms=`echo ${DOMAIN} | python cmds.py`
 fi
-echo "current number of managed servers is $num_ms" >> scalingAction.log
+echo "current number of managed servers is $num_ms" >> scalingActionUp.log
 
 # Cleanup cmds.py
 [ -e cmds.py ] && rm cmds.py
@@ -211,7 +205,7 @@ else
   fi
 fi
 
-echo "new_ms is $new_ms" >> scalingAction.log
+echo "new_ms is $new_ms" >> scalingActionUp.log
 
 request_body=$(cat <<EOF
 {
@@ -220,7 +214,7 @@ request_body=$(cat <<EOF
 EOF
 )
 
-echo "request_body: $request_body" >> scalingAction.log
+echo "request_body: $request_body" >> scalingActionUp.log
 
 content_type="Content-Type: application/json"
 accept_resp_type="Accept: application/json"
@@ -233,30 +227,30 @@ if [ ${INTERNAL_OPERATOR_CERT} ]
 then
   echo ${INTERNAL_OPERATOR_CERT} | base64 --decode >  $pem_filename
 else
-  echo "Operator Cert File not found" >> scalingAction.log
+  echo "Operator Cert File not found" >> scalingActionUp.log
   exit 1
 fi
 
 # Operator Service REST URL for scaling
 operator_url="https://$operator_service_name.$operator_namespace.svc.cluster.local:$port/operator/v1/domains/$wls_domain_uid/clusters/$wls_cluster_name/scale"
-echo "operator_url: $operator_url" >> scalingAction.log
+echo "operator_url: $operator_url" >> scalingActionUp.log
 
 # send REST request to Operator
 if [ -e $pem_filename ]
 then
   result=`curl --cacert $pem_filename -X POST -H "$content_type" -H "$requested_by" -H "$authorization" -d "$request_body" $operator_url`
 else
-  echo "Operator PEM formatted file not found" >> scalingAction.log
+  echo "Operator PEM formatted file not found" >> scalingActionUp.log
   exit 1
 fi
 
 if [ $? -ne 0 ]
 then
-  echo "Failed scaling request to WebLogic Operator" >> scalingAction.log
-  echo $result >> scalingAction.log
+  echo "Failed scaling request to WebLogic Operator" >> scalingActionUp.log
+  echo $result >> scalingActionUp.log
   exit 1
 fi
-echo $result >> scalingAction.log
+echo $result >> scalingActionUp.log
 
 # Cleanup generated operator PEM file
 [ -e $pem_filename ] && rm $pem_filename 
